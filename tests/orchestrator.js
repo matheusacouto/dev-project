ac46c6fc35fc4db111607ee6d9d22a5aa1b30b8a
Matheus Couto <fl_matheusacouto@hotmail.com>
2025-06-24 07:28:02 -0300

    feat: create and use orchestrator.createUser()

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,6 +1,9 @@
 import retry from 'async-retry'
+import { faker } from '@faker-js/faker'
+
 import database from 'infra/database'
 import migrator from 'models/migrator.js'
+import user from 'models/user'
 
 async function waitForAllServices() {
     await waitForWebServer()
@@ -28,10 +31,21 @@ async function runPendingMigrations() {
     await migrator.runPendingMigrations()
 }
 
+async function createUser(userObject) {
+    return await user.create({
+        username:
+            userObject.username ||
+            faker.internet.username().replace(/_.-/g, ''),
+        email: userObject.email || faker.internet.email(),
+        password: userObject.password || 'validpassword',
+    })
+}
+
 const orchestrator = {
     waitForAllServices,
     clearDatabase,
     runPendingMigrations,
+    createUser,
 }
 
 export default orchestrator
